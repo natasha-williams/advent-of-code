4671dc0f3c98171bff5d78caddc3e50746ecd3bd
Natasha Williams <natashamary_williams@hotmail.co.uk>
2019-12-09 13:58:26 +0000

    Day 4

---
+++ 2019/04/script.py
@@ -0,0 +1,53 @@
+
+MAX_LENGTH = 6
+
+
+def is_ascending(value):
+    data = sorted(value)
+    return ''.join(data) == value
+
+
+def has_repeat(value, limit=None):
+    data = []
+
+    for item in set(value):
+        data.append([x for x in value if x == item])
+
+    groups = [len(item) for item in data]
+    repeated_items = filter(lambda item: item > 1, groups)
+
+    if limit is not None:
+        repeated_items = filter(lambda item: item == 2, repeated_items)
+
+    return len(repeated_items) > 0
+
+
+def get_file_text():
+    text = open('input.txt').read()
+    return [int(item) for item in text.split('-')]
+
+
+data = get_file_text()
+numbers = range(data[0], data[1])
+total_valid = 0
+total_consecutive_valid = 0
+
+for item in numbers:
+    value = str(item)
+
+    if len(value) != MAX_LENGTH:
+        continue
+
+    if not is_ascending(value):
+        continue
+
+    if not has_repeat(value):
+        continue
+
+    total_valid += 1
+
+    if has_repeat(value, limit=2):
+        total_consecutive_valid += 1
+
+print('Part 1: {}'.format(total_valid))
+print('Part 2: {}'.format(total_consecutive_valid))
