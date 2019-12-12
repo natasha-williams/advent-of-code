99b4102c950b97d10747c3eb844a696e516d8f35
Natasha Williams <natashamary_williams@hotmail.co.uk>
2019-12-12 15:17:01 +0000

    Day 7

---
+++ 2019/07/script.py
@@ -0,0 +1,34 @@
+
+import path
+
+from itertools import permutations
+
+from intcode import IntcodeGenerator
+
+
+class LoopIntcodeGenerator(IntcodeGenerator):
+    OPTCODE_EXIT_MODES = (4, 99)
+
+
+def get_max_signal(phases, feedback_mode=False):
+    signals = []
+
+    for permutation in phases:
+        cls = LoopIntcodeGenerator if feedback_mode else IntcodeGenerator
+        generators = [cls.from_phase(item) for item in permutation]
+        input_value = 0
+
+        while not generators[-1].has_complete:
+            for generator in generators:
+                input_value = generator.run(input_value)
+
+        signals.append(input_value)
+
+    return max(signals)
+
+
+phases_1 = permutations(range(0, 5))
+phases_2 = permutations(range(5, 10))
+
+print('Part 1: {}'.format(get_max_signal(phases_1)))
+print('Part 2: {}'.format(get_max_signal(phases_2, feedback_mode=True)))
